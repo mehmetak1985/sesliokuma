0d01cbc2fb951e90173b84a732168aac38db4273
minik-okur <tutunistan@gmail.com>
2026-02-26 09:22:48 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,39 @@
-self.addEventListener('fetch', function(event) {
-  // PWA onayı için gerekli boş dinleyici
+const CACHE_NAME = 'minik-okur-v1';
+const ASSETS = [
+  '/sesliokuma/',
+  '/sesliokuma/index.html',
+  '/sesliokuma/manifest.json',
+  '/sesliokuma/icon-192.png',
+  '/sesliokuma/icon-512.png',
+  '/sesliokuma/screenshot-mobile.png',
+  '/sesliokuma/screenshot-desktop.png'
+];
+
+// Kurulum: Dosyaları önbelleğe al
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Aktivasyon: Eski önbellekleri temizle
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// İstekleri Yakalama: Önce önbelleğe bak, yoksa internetten getir
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
